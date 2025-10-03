b115f5e802b5d6ceb43385d2ef50b0b84c1c437b
d1n4ara <dandararamoscosta@gmail.com>
2025-10-03 12:25:00 -0300

    fix(banco-de-dados):Organizando pendencias

--- dbStructure.sql
+++ dbStructure.sql
@@ -1,10 +1,7 @@
+DROP DATABASE if exists bdOberon;
 CREATE DATABASE bdOberon;
 
 USE bdOberon;
-
--- ==========================
--- Tabelas Principais
--- ==========================
 
 CREATE TABLE Empresa (
     idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
@@ -23,7 +20,7 @@ CREATE TABLE Funcionario (
     nome VARCHAR(45) NOT NULL,
     cpf CHAR(11) UNIQUE NOT NULL,
     email VARCHAR(45) UNIQUE NOT NULL,
-    senha VARCHAR(45) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
     fkTipoUsuario INT,
     fkEmpresa INT,
     FOREIGN KEY (fkTipoUsuario) REFERENCES TipoUsuario(idTipoUsuario),
@@ -32,11 +29,13 @@ CREATE TABLE Funcionario (
 
 CREATE TABLE Maquina (
     idMaquina INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
     hostname VARCHAR(45) NOT NULL,
     modelo VARCHAR(45),
     macAddress VARCHAR(45),
+    ip VARCHAR(45) NOT NULL,
     sistemaOperacional VARCHAR(45),
-    statusAtivo VARCHAR(45),
+    status VARCHAR(45),
     fkEmpresa INT,
     FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa)
 );
@@ -63,7 +62,6 @@ CREATE TABLE MaquinaComponente (
 CREATE TABLE Parametro (
     idParametro INT PRIMARY KEY AUTO_INCREMENT,
     limite FLOAT NOT NULL,
-    tipoParametro VARCHAR(45),
     fkEmpresa INT,
     fkMaquinaComponente INT,
     FOREIGN KEY (fkEmpresa) REFERENCES Empresa(idEmpresa),
@@ -92,3 +90,49 @@ CREATE TABLE Alerta (
     FOREIGN KEY (fkMaquina) REFERENCES Maquina(idMaquina)
 );
 
+CREATE OR REPLACE VIEW viewDadosMaquina AS
+SELECT 
+	m.idMaquina,
+    m.nome,
+    m.hostname,
+    m.modelo,
+    m.ip,
+    m.sistemaOperacional,
+    e.razaoSocial,
+    m.macAddress
+FROM Maquina as m 
+JOiN Empresa as e 
+ON m.fkEmpresa = e.idEmpresa;
+
+CREATE OR REPLACE VIEW ViewParametrosMaquina AS 
+SELECT
+    MC.idMaquinaComponente,
+    C.funcaoMonitorar,
+    C.unidadeMedida,
+	COALESCE(PE.limite, PP.limite) AS limite_aplicado,
+
+    CASE C.tipoComponente
+        WHEN 'CPU' THEN CAST(MC.nucleosThreads AS CHAR)
+        WHEN 'RAM' THEN CONCAT(CAST(MC.capacidadeGb AS CHAR), ' GB')
+        WHEN 'Disco Duro' THEN CONCAT(CAST(MC.capacidadeGb AS CHAR), ' GB (', MC.tipoDisco, ')')
+        ELSE 'N/A'
+    END AS atributo_componente,
+    CASE
+        WHEN PE.limite IS NOT NULL THEN 'Específico'
+        WHEN PP.limite IS NOT NULL THEN 'Padrão'
+        ELSE 'Não Definido'
+    END AS tipo_parametro_usado,
+    C.tipoComponente,
+	M.idMaquina
+FROM
+    Maquina AS M
+JOIN
+    MaquinaComponente AS MC ON M.idMaquina = MC.fkMaquina
+JOIN
+    Componente AS C ON MC.fkComponente = C.idComponente
+LEFT JOIN
+    Parametro AS PE ON PE.fkMaquinaComponente = MC.idMaquinaComponente
+    AND PE.fkEmpresa IS NULL
+LEFT JOIN
+    Parametro AS PP ON PP.fkEmpresa = M.fkEmpresa
+    AND PP.fkMaquinaComponente IS NULL;
